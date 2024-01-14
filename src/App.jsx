e93e6ed4d88555e9eac9cfd7ab5275716661f491
huzaifak08 <hk7928042@gmail.com>
2024-01-14 21:09:39 +0500

    Hooks concept in Counter App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,32 @@
-import Chai from './Chai';
+import { useState } from "react";
 
 function App() {
+  // let counter = 0;  Here comes the hooks to update the UI
+
+  let [counter, setCounter] = useState(0); // counter is variable and setCounter is a function
+
+  const addValue = ()=>{
+    // counter = counter + 1;
+
+    setCounter(counter+1);
+  }
+
+  const removeValue = ()=>{
+    // counter = counter - 1;
+
+    setCounter(counter-1);
+  }
+
   return (
     <>
-    <h1>Chai aur React with vite</h1>
-    <Chai/>
+    <h1>Counter App Example</h1>
+
+    <h3>Counter Value: {counter}</h3>
+
+    <button onClick={addValue}>Add Value</button>
+
+    <br />
+    <button onClick={removeValue}>Remove Value</button>
     </>
   )
 }
--- src/Chai.jsx
+++
@@ -1,7 +0,0 @@
-function Chai(){
-    return(
-        <h1>Lets have a meeting on chai</h1>
-    )
-}
-
-export default Chai
