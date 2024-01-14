f6b3104bb40b7baa13af4a633becd1c7081effff
huzaifak08 <hk7928042@gmail.com>
2024-01-14 20:07:50 +0500

    React components

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import Chai from './Chai';
+
+function App() {
+  return (
+    <>
+    <h1>Chai aur React with vite</h1>
+    <Chai/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Chai.jsx
@@ -0,0 +1,7 @@
+function Chai(){
+    return(
+        <h1>Lets have a meeting on chai</h1>
+    )
+}
+
+export default Chai
