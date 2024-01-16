546e8cd9fe24209fafba6d5f844ccb6e2a6321d3
huzaifak08 <hk7928042@gmail.com>
2024-01-16 17:55:44 +0500

    UseEffect UseRef UseCallBack done Allhamdullillah

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,105 @@
-import { useState } from "react";
+import { useState, useCallback, useEffect, useRef } from "react";
 
 function App() {
-  let [color, setColor] = useState("olive");
+  let [lenght, setLength] = useState(8);
+  let [noAllowed, setNoAllowed] = useState(false);
+  let [charAllowed, setCharAllowed] = useState(false);
+  let [password, setPassword] = useState("");
+
+  // UseRef hook:
+  const passRef = useRef(null);
+
+  // Optimize and save the values in Chache
+  const passwordGenerator = useCallback(() => {
+    let pass = "";
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuwxyz";
+
+    if (noAllowed) str += "0123456789";
+    if (charAllowed) str += "@(){}#!$%^&*";
+
+    for (let i = 1; i <= lenght; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+
+      pass += str.charAt(char);
+    }
+
+    setPassword(pass);
+  }, [lenght, noAllowed, charAllowed, setPassword]);
+
+  // Run the Project (init state)
+  useEffect(() => {
+    passwordGenerator();
+  }, [lenght, noAllowed, charAllowed, passwordGenerator]);
+
+  const copyPassToClip = useCallback(() => {
+    passRef.current?.select(); // change color of copied text
+    // passRef.current?.setSelectionRange(0, 4);
+
+    window.navigator.clipboard.writeText(password);
+  }, [password]);
 
   return (
-    <div
-      className="w-full h-screen duration-200"
-      style={{ backgroundColor: color }}
-    >
-      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
-        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-lg">
+    <>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-700">
+        <h1 className="text-white text-center">Password Generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4 my-3">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="Password"
+            readOnly
+            ref={passRef}
+          />
           <button
-            className="outline-none px-4 py-1 rounded-full text-white"
-            style={{ backgroundColor: "red" }}
-            onClick={() => setColor("red")}
+            className="outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0"
+            onClick={copyPassToClip}
           >
-            Red
-          </button>
-
-          <button
-            className="outline-none px-4 py-1 rounded-full text-white"
-            style={{ backgroundColor: "green" }}
-            onClick={() => setColor("green")}
-          >
-            Green
-          </button>
-
-          <button
-            className="outline-none px-4 py-1 rounded-full text-black"
-            style={{ backgroundColor: "yellow" }}
-            onClick={() => setColor("yellow")}
-          >
-            Yellow
-          </button>
-
-          <button
-            className="outline-none px-4 py-1 rounded-full text-white"
-            style={{ backgroundColor: "blue" }}
-            onClick={() => setColor("blue")}
-          >
-            Blue
+            Copy
           </button>
         </div>
+
+        <div className="flex text-sm gap-x-2">
+          <div className="flex items-center gap-x-1">
+            <input
+              type="range"
+              min={8}
+              max={100}
+              value={length}
+              className="cursor-pointer"
+              onChange={(e) => {
+                setLength(e.target.value);
+              }}
+            />
+            <label>Lenght: {lenght}</label>
+          </div>
+
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={noAllowed}
+              id="numberInput"
+              onChange={(e) => {
+                setNoAllowed((prev) => !prev);
+              }}
+            />
+            <label>Numbers</label>
+          </div>
+
+          <div className="flex items-center gap-x-1">
+            <input
+              type="checkbox"
+              defaultChecked={charAllowed}
+              id="characterInput"
+              onChange={(e) => {
+                setCharAllowed((prev) => !prev);
+              }}
+            />
+            <label>Characters</label>
+          </div>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
